3c39943df595a87e7598877e1bae06e668096ee1
Mohammad Maghsoudi <maghsoudismtp@gmail.com>
2021-06-26 20:18:05 -0400

    first from my mac

---
+++ example2.py
@@ -0,0 +1,142 @@
+from textdetection.detection import Detector, Thresholds
+import pandas as pd
+import time
+import pickle
+import logging
+import numpy as np
+import random
+from multiprocessing import Pool, Process
+import os
+from matplotlib import pyplot as plt
+
+logging.basicConfig(filename='outputs/mylogs/mylogs.log', format='%(asctime)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logging.info(f'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< NEW START  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+
+def detect_process_log(data_with_path, k=10, threshold=0.4, multiprocess=False, chunk=20):
+    detector = Detector(k=k, threshold=threshold)
+    # logging.info(f"<<<<<<<<<<<<<<<<<<<<<<<<<<<  detect_process_log  >>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    # logging.info(f"Detector(k={k}, theshold={threshold})")
+    start_time = time.time()
+    if multiprocess:
+        detector.multi_process(data_with_path[0], chunk=chunk, sless=0)
+    elif not multiprocess:
+        detector.process(data_with_path[0])
+    elapsed_time = time.time() - start_time
+    logging.info(
+        f"time\t>>>>\t{elapsed_time} s \tnum of detections: {len(detector.history)}\n\tdata: {data_with_path[1]}\t"
+        f"len(data)={len(data_with_path[0])}\n\tDetector(k={k}, theshold={threshold} \n\t positions: {detector.history}")
+    detector.print_result()
+    print(f"time elapsed : : : {elapsed_time}")
+    return detector
+
+
+df_list_yelp = []
+K = 5
+THRESHOLD = 0.1
+all_result = []
+SAVE_ALL_RESULTS = False
+SAVE_PICKLE_PLOT = False
+if True:
+    k_values = []
+    num_of_detection_list = []
+    thresholdsss = []
+    file_name = f"glue"
+    start_time = time.time()
+    data_path = f"test_for_attack_30.csv"
+    data_path = "../data/benign/glue.csv" #sentence
+    data_path = "../data/benign/yelp_review_full_csv/mytest.csv"
+    # for i in np.linspace(0.1, 0.4, num=20):
+    data_with_path = [pd.read_csv(data_path)['text'][:1000], data_path]
+    for z in [10]:#, 50, 60]: #[10, 11, 12, 13, 14, 15]:
+        for i in [0.41, 0.42, 0.44, 0.46]: #[0.1, 0.2, 0.3, 0.4, 0.5]:#, 0.17, 0.22, 0.27, 0.3]:
+            # K = random.randint(6, 30)
+            K= z
+            THRESHOLD = i
+            # THRESHOLD = random.uniform(0.09, 0.42)
+            # file_name = f"yelp"
+            # data_path = f"../data/benign/yelp_review_full_csv/new_versions/{i}_" + file_name + ".csv"#text
+            # data_path = f"../data/benign/glue_series/{i}_" + file_name + ".csv"#text
+            # df_list_yelp.append([pd.read_csv(data_path)['text'], data_path])
+            # data_with_path = [pd.read_csv(data_path)['sentence'], data_path]
+            detector_inside = detect_process_log(data_with_path, k=K, threshold=THRESHOLD,
+                                                  multiprocess=True, chunk=80)
+            # for ii in detector_inside.history:
+            #     print(f"\nindex {ii}\n",data_with_path[0].iloc[ii])
+            print(f">>>>>>>>>>> {i} process finished.")
+            k_values.append(K)
+            num_of_detection_list.append(len(detector_inside.history))
+            thresholdsss.append(THRESHOLD)
+            # THRESHOLD += 0.02
+            # K += 2
+    elapsed_time = time.time() - start_time
+    logging.info(f"<<<<<<<<<<<<<<< time passed: {elapsed_time} >>>>>>>>>>>>>>>")
+
+    # K += 1
+    # THRESHOLD += 0.01
+    result = [file_name, K, THRESHOLD, k_values, thresholdsss, num_of_detection_list]
+    if SAVE_ALL_RESULTS:
+        if not os.path.exists(f"outputs/pickle_list"):
+            os.makedirs(f"outputs/pickle_list")
+        str_time = time.strftime("%m-%d_%H-%M")
+        file_name_pickle = f"outputs/pickle_list/pickle_{str_time}.txt"
+        with open(file_name_pickle, "wb") as fp:  # Pickling
+            pickle.dump(result, fp)
+        logging.info(f"The list of results was saved into: {file_name_pickle}")
+
+    if SAVE_PICKLE_PLOT:
+        # str_time = time.strftime("%m-%d_%H-%M-%s")
+        f, ax = plt.subplots(1)
+        ax.plot(result[3], result[5])
+        # ax.set_ylim(ymin=0)
+        # ax.set_xlim(xmin=0)
+        plt.xlabel(f"K")
+        plt.ylabel("#Num of detections")
+        plt.title(f"threshold={THRESHOLD}")
+        # plt.plot(results_of_k, results_thresholds)
+        plt.show()
+
+        file_name_plot_PDF = f"outputs/pickle_list/plot_k_{result[1]}_threshold_{'%.2f' % result[2]}_{str_time}.pdf"
+        if not os.path.exists(f"outputs/pickle_list"):
+            os.makedirs(f"outputs/pickle_list")
+        f.savefig(file_name_plot_PDF, bbox_inches='tight')
+        logging.info(f"The plot was saved into:  {file_name_plot_PDF}")
+
+
+def Insert_row(row_number, df, row_value):
+    # Starting value of upper half
+    start_upper = 0
+
+    # End value of upper half
+    end_upper = row_number
+
+    # Start value of lower half
+    start_lower = row_number
+
+    # End value of lower half
+    end_lower = df.shape[0]
+
+    # Create a list of upper_half index
+    upper_half = [*range(start_upper, end_upper, 1)]
+
+    # Create a list of lower_half index
+    lower_half = [*range(start_lower, end_lower, 1)]
+
+    # Increment the value of lower half by 1
+    lower_half = [x.__add__(1) for x in lower_half]
+
+    # Combine the two lists
+    index_ = upper_half + lower_half
+
+    # Update the index of the dataframe
+    df.index = index_
+
+    # Insert a row at the end
+    df.loc[row_number] = row_value
+
+    # Sort the index labels
+    df = df.sort_index()
+
+    # return the dataframe
+    return df
--- textdetection/detection.py
+++ textdetection/detection.py
@@ -1,16 +1,57 @@
 import numpy as np
-from fuzzywuzzy import fuzz
 from tqdm import tqdm
-from scipy.spatial.distance import cdist
+from scipy.spatial.distance import cdist, pdist, squareform
+from Levenshtein import ratio
 import pandas as pd
-from multiprocessing import Process, Queue
-from time import sleep
+from multiprocessing import Process, Queue, Pool
+import logging
+
+
+def worker_pool_thresholds(input_list):
+    data1 = input_list[0]
+    data2 = input_list[1]
+    k = input_list[2]
+    new_distance_mat = cdist(data1, data2, lambda a, b: 1 - ratio(a[0], b[0]))
+    new_distance_mat = np.sort(new_distance_mat, axis=-1)
+    distance_mat_k = new_distance_mat[:, :k]
+    return distance_mat_k
+
+#
+# def process_worker_detector(input_list):
+#     queries = np.array(input_list[0])[:, None]
+#     dists = cdist(queries, np.reshape(input_list[1], (-1, 1)), lambda a, b: 1 - ratio(a[0], b[0])).reshape(-1)
+#     return dists
+
+
+def get_nearest_queries(queries, query, k, pr: bool = False):
+    """
+    :param queries: DataFrame(text) or list of text
+    :param query: one single string
+    :param k: int for knn
+    :param pr: to only print the results set True
+    :return: k_index, k_nearest_dists, k_avg_dist
+    """
+    k = k
+
+    queries = np.array(queries)[:, None]
+    dists = cdist(queries, np.reshape(query, (-1, 1)),
+                  lambda a, b: 1 - ratio(a[0], b[0])).reshape(-1)
+    k_index = dists.argpartition(k-1, axis=0)[:k]
+    k_nearest_dists = np.partition(dists, k - 1)[:k, None]
+    # # transformed_strings
+    # print(k_nearest_dists)
+    k_avg_dist = np.mean(k_nearest_dists)
+    # print(k_i)
+    if pr:
+        pr(k_index, "\n", k_nearest_dists, "\n", k_avg_dist)
+    else:
+        return k_index, k_nearest_dists, k_avg_dist
 
 
 class Detector:
     """Create a new object of the class and call process() from the object"""
 
-    def __init__(self, k: int = 25, threshold: int = 15):
+    def __init__(self, k: int = 25, threshold: float = 15):
         """
         k is the number of nearest neighbors of each query that are accounted for detecting an attack
         threshold is a number between 0 to 100 and shows that if the average of distances of k neighbors
@@ -23,13 +64,16 @@ class Detector:
         self.list_of_buffers = []
         self.chunk_size = 100
         self.list_of_processes = []
+        self.length_of_input = 0
         self.history_attack = []
         self.history = []  # detected attacks
         self.detected_dists = []  # detected knn distances
+        self.str_distance = self.str_distance_ratio
+        self.pdist_values = None
 
     @staticmethod
-    def str_distance(a, b):
-        return 100 - fuzz.partial_ratio(a, b)
+    def str_distance_ratio(a, b):
+        return 1 - ratio(a, b)
 
     @staticmethod
     def __check_type(data):
@@ -51,10 +95,53 @@ class Detector:
                 f"Input cannot be a string, it is expected to be a list of strings or"
                 f" one column (DataFrame) of strings")
 
-    def process(self, queries, reset: bool = False):
+    def once_process(self, queries, reset: bool = False):
+        """calculate pdist once for the whole data and find knn for each query
+        using right indexes of the pdist output.
+        Just for large data without attack (large and benign data) is faster,
+        otherwise process method is much faster"""
         if reset:
             self.__init__(self.K, self.threshold)
         self.__check_type(queries)
+        k = self.K
+        previous_attack_index = 0
+        # if metric == "ratio":
+        #     self.str_distance = self.str_distance_ratio
+        self.length_of_input = len(queries)
+        queries = np.array(queries)[:, None]
+        query_index_reset = 0
+        self.pdist_values = squareform(pdist(queries, lambda a, b: self.str_distance(a[0], b[0])))
+        for query_index in tqdm(range(len(queries))):
+            query_index_reset += 1
+            if query_index_reset <= self.K:
+                self.num_queries += 1
+                continue
+            dists = self.pdist_values[query_index, previous_attack_index:query_index]
+            k_nearest_dists = np.partition(dists, k - 1)[:k, None]
+            k_avg_dist = np.mean(k_nearest_dists)
+
+            self.num_queries += 1
+            is_attack = k_avg_dist < self.threshold
+            if is_attack:
+                previous_attack_index = query_index
+                self.history.append(self.num_queries)
+                query_index_reset = 0
+                self.detected_dists.append(k_avg_dist)
+                self.clear_memory()
+
+    def process(self, queries, reset: bool = False):
+        """
+
+        :param queries: list of text or a pandas column of text
+        :param reset: True for reseting the results in the class
+        :return:
+        """
+        if reset:
+            self.__init__(self.K, self.threshold)
+        self.__check_type(queries)
+        # if metric == "ratio":
+        #     self.str_distance = self.str_distance_ratio
+        self.length_of_input = len(queries)
         for query in tqdm(queries):
             self.process_query(query)
 
@@ -78,17 +165,55 @@ class Detector:
             self.detected_dists.append(k_avg_dist)
             self.clear_memory()
 
-    def multi_process(self, queries, chunk: int = 75, reset: bool = False, sless: int = 0):
+    def multi_process(self, queries, chunk: int = 75, reset: bool = False, sless=0): #, pooling_process=0):
         if not isinstance(chunk, int) or not isinstance(reset, bool):
             raise ValueError(f"reset should be bool and chunk should be int!")
         if reset:
             self.__init__(self.K, self.threshold)
+        # if metric == "ratio":
+        #     self.str_distance = self.str_distance_ratio
         self.chunk_size = chunk
         self.__check_type(queries)
+        self.length_of_input = len(queries)
+        # if not pooling_process:
         for query in tqdm(queries):
             self.multi_process_query(query, sless)
+        # elif pooling_process:
+        #     for query in tqdm(queries):
+        #         self.multi_process_query_pooling(query, pooling_process)
+        # else:
+        #     exit(1)
 
-    def multi_process_query(self, query, sless):
+    # def multi_process_query_pooling(self, query, num_process):
+    #     if len(self.buffer) < self.K:
+    #         self.buffer.append(query)
+    #         self.num_queries += 1
+    #         return False
+    #
+    #     k = self.K
+    #     input_lists = []
+    #     px = len(self.buffer) // num_process if len(self.buffer) > num_process else 2
+    #     px = 75
+    #     for i in range(0, len(self.buffer), px):
+    #         input_lists.append([self.buffer[i:i + px], query])
+    #
+    #     with Pool(num_process) as p:
+    #         distances = p.map(process_worker_detector, input_lists)
+    #
+    #     dists = np.concatenate(distances)
+    #     k_nearest_dists = np.partition(dists, k - 1)[:k, None]
+    #     k_avg_dist = np.mean(k_nearest_dists)
+    #
+    #     self.buffer.append(query)
+    #     self.num_queries += 1
+    #
+    #     is_attack = k_avg_dist < self.threshold
+    #     if is_attack:
+    #         self.history.append(self.num_queries)
+    #         self.detected_dists.append(k_avg_dist)
+    #         self.clear_memory()
+
+    def multi_process_query(self, query, sless=0):
         if not self.list_of_buffers and len(self.buffer) < self.K:
             self.buffer.append(query)
             self.num_queries += 1
@@ -158,19 +283,23 @@ class Detector:
 
         return epochs
 
-    def print_result(self):
+    def print_result(self, show_avg=False):
         print("\n")
         epochs = self.get_detections()
         num_attacks = len(epochs)
         if num_attacks == 0:
-            print("\n\033[32m---No attack is detected!---\033[00m")
-            print("\033[38;5;105mNum detections:", num_attacks, "\033[00m")
+            print("\n\033[32m\t\t---->>No attack is detected!<<----\033[00m")
+            print("\033[34m#Num of detections:\t", num_attacks, "\033[00m")
         else:
-            print("\033[31m--->>>ATTACK DETECTED!<<<---\033[00m")
-            print("\033[38;5;105mNum detections:", num_attacks, "\033[00m")
+            print("\033[31m\t\t--->>>ATTACK DETECTED!<<<---\033[00m")
+            print("\033[38;5;105mNum detections:\t", num_attacks, "\033[00m")
 
-        print("Queries per detection:", epochs)
-        print("i-th query that caused detection:", self.history)
+        print("Queries per detection:\t", epochs)
+        print("i-th query that caused detection:\t", self.history)
+        print("\033[36m#Num of queries\t>>>>>>>>\t", self.length_of_input, "\033[00m")
+        if show_avg:
+            print("Avg. distance Detected :\t", self.detected_dists,
+                  "\n\t\t---------------END---------------")
 
 
 class Thresholds:
@@ -178,35 +307,54 @@ class Thresholds:
         self.list_of_k = []
         self.list_of_thresholds = []
         self.saved_history = []
+        self.str_distance = self.str_distance_ratio
 
     @staticmethod
-    def str_distance(a, b):
-        return 100 - fuzz.partial_ratio(a, b)
-
-    # def chunks(lst, n):
-    #     """Yield successive n-sized chunks from lst."""
-    #     for i in range(0, len(lst), n):
-    #         yield lst[i:i + n]
+    def str_distance_ratio(a, b):
+        return 1 - ratio(a, b)
 
     def calculate_thresholds(self, data, k, chunk: int = 100, up_to_k: bool = False,
-                             multiprocess=False, num_process: int = 4, percentile = 0.1):
+                             multiprocess="singleprocess", num_process: int = 4, percentile=0.1, pair="cdist"):
+
+        logging.info(f"calculate_thresholds(data, k={k}, chunk={chunk}, up_to_k={up_to_k}, multiprocess={multiprocess},"
+                     f" num_process= {num_process}, percentile={percentile}, pair={pair})")
+        logging.info(f"Input data length: {len(data)}")
+        # if metric == "ratio":
+        #     self.str_distance = self.str_distance_ratio
         data = np.reshape(data.values, (-1, 1))
         distances = []
-        process_list = []
 
-        if not multiprocess:
-            print('single processing')
-            for i in tqdm(range(0, len(data), chunk)):
-                new_distance_mat = cdist(data[i:i + chunk, :], data, lambda a, b: Detector.str_distance(a[0], b[0]))
-                new_distance_mat = np.sort(new_distance_mat, axis=-1)
+        if multiprocess == "singleprocess":
+            if pair == "pdist":
+                print('Single processing - pdist - without loading - please wait...')
+                new_distance_mat = pdist(data, lambda a, b: self.str_distance(a[0], b[0]))
+                new_distance_mat = np.sort(squareform(new_distance_mat), axis=-1)
+                # I think it's better to use new_distance_mat[:, 1:k+1] but I need to test it before changing
                 distance_mat_k = new_distance_mat[:, :k]
 
                 distances.append(distance_mat_k)
+            else:
+                print('Single processing - cdist')
+                for i in tqdm(range(0, len(data), chunk)):
+                    new_distance_mat = cdist(data[i:i + chunk, :], data, lambda a, b: self.str_distance(a[0], b[0]))
+                    new_distance_mat = np.sort(new_distance_mat, axis=-1)
+                    distance_mat_k = new_distance_mat[:, :k]
 
-        elif multiprocess:
+                    distances.append(distance_mat_k)
+
+        elif multiprocess == "multiprocess":
             queue = Queue()
+            chunk_changed = len(data) % chunk < num_process and len(data) % chunk != 0
+            while len(data) % chunk < num_process and len(data) % chunk != 0:
+                chunk += 1
+            if chunk_changed:
+                print(f"Warning: chunk size has changed to {chunk}")
+            parts = len(data) // chunk if len(data) % chunk == 0 else (len(data) // chunk) + 1
+            logging.info(f"{parts} Parts \t"
+                         f"~{num_process} Processes for each part")
             print(
-                f"\n\033[01;33m{len(data) // chunk + 1} Parts...\n~{num_process} Processes for each part\n\033[01;36mWait...\n\033[00m")
+                f"\n\033[01;33m{parts} Parts...\n"
+                f"~{num_process} Processes for each part\n\033[01;36mWait...\n\033[00m")
 
             for i in range(0, len(data), chunk):
                 list_of_processes = []
@@ -214,46 +362,36 @@ class Thresholds:
                 px = len(new_data) // num_process
                 for inx in range(0, len(new_data), px):
                     tmp = new_data[inx:inx + px, :]
-                    self.saved_history.append([tmp, inx, inx + px, px])
+                    # self.saved_history.append([tmp, inx, inx + px, px])
 
                     prc = Process(target=self.__process_worker_thresholds, args=(tmp, data, k, queue,))
                     prc.start()
                     list_of_processes.append(prc)
 
-                for each_pr in tqdm(list_of_processes, leave=True):
+                for each_pr in tqdm(list_of_processes):
                     each_pr.join()
-                while not queue.empty():
                     distances.append(queue.get())
 
-            print("\n\033[38;5;105mWaiting for merging outputs.\033[00m")
+                # while not queue.empty():
+                #     distances.append(queue.get())
 
-        else:
-            # todo
-            print("multiprocess= argument should set bool, e.g. multiprocess=True")
-            exit(1)
-            queue = Queue()
+                logging.info(f"queue getting, len(distances) : {len(distances)}")
+        elif multiprocess == "pooling":
+            print(f"Pool: {num_process}")
+            input_lists = []
+
             for i in tqdm(range(0, len(data), chunk)):
-                tmp = data[i:i + chunk, :]
-                prc = Process(target=self.__process_worker_thresholds, args=(tmp, data, k, queue,))
-                # sleep(1)
-                prc.start()
-                process_list.append(prc)
+                input_lists.append([data[i:i + chunk, :], data, k])
 
-                # sleep()
-            # print("\033[38;5;105m", len(list_of_processes), " Processe(s) is/are created.\033[00m\n-----")
+            with Pool(num_process) as p:
+                distances = p.map(worker_pool_thresholds, input_lists)
+        else:
+            print(f"multiprocess argument should be defined properly. {multiprocess} is not defined!")
+            exit(1)
 
-            print("\033[38;5;105mWaiting for merging outputs.\033[00m")
-            sleep(2)
-            # for pp in process_list:
-            #     if pp.is_alive():
-            #         pp.join()
-            #     else:
-            #         continue
-            process_list[0].join()
-            while not queue.empty():
-                distances.append(queue.get())
-
+        print("\033[38;5;105mWaiting for merging outputs.\033[00m")
         distance_matrix = np.concatenate(distances, axis=0)
+        logging.info(f"Matrix length >>>>>>>> {len(distance_matrix)}")
         print("Matrix length >>>>>>>> ", len(distance_matrix))
 
         def __last_assigning():
